a942ca7929b3343b04ac3f08adf2f76eb62f01a0
isaacsaless <isaacnascimentosales@gmail.com>
2025-03-18 19:15:18 -0300

    18/03 bracket update

--- src/components/torneios/PingPong/PingPongAvancadoMasc.tsx
+++ src/components/torneios/PingPong/PingPongAvancadoMasc.tsx
@@ -2,20 +2,80 @@ import Image from "next/image";
 import LogoAdm from "@/app/assets/logo-adm.png";
 import LogoDs from "@/app/assets/logo-ds.png";
 import LogoSister from "@/app/assets/logo-sister.png";
-import QuestionMark from "@/app/assets/question-mark.png";
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import confetti from "canvas-confetti";
 
 export default function VoleiAvancadoMasc() {
+    const winnerRef = useRef<HTMLLIElement>(null);
+      const [isShaking, setIsShaking] = useState(false);
+    
+      const handleMouseEnter = () => setIsShaking(true);
+      const handleMouseLeave = () => setIsShaking(false);
+  
+    const animateConfetti = () => {
+      if (!winnerRef.current) return;
+  
+      const rect = winnerRef.current.getBoundingClientRect();
+  
+      const myCanvas = document.createElement("canvas");
+      myCanvas.style.position = "fixed";
+      myCanvas.style.top = "0px";
+      myCanvas.style.left = "0px";
+      myCanvas.style.pointerEvents = "none";
+      myCanvas.style.width = "100%";
+      myCanvas.style.height = "100%";
+      myCanvas.style.zIndex = "1000";
+      document.body.appendChild(myCanvas);
+  
+      const myConfetti = confetti.create(myCanvas, {
+        resize: true,
+        useWorker: true,
+      });
+  
+      const x = (rect.left + rect.width / 2) / window.innerWidth;
+      const y = (rect.top + rect.height / 2) / window.innerHeight;
+  
+      myConfetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x, y },
+        colors: ["#5AA91B", "#FEC847"],
+        disableForReducedMotion: true,
+      });
+  
+      setTimeout(() => {
+        document.body.removeChild(myCanvas);
+      }, 3000);
+    };
+  
+    useEffect(() => {
+      animateConfetti();
+    }, []);
+
   return (
     <div className="overflow-x-scroll min-h-[680px] overflow-y-hidden text-nowrap">
       <div className="flex mr-3 mt-16">
         <ol className="flex flex-1 flex-col justify-around mr-5 round">
-          <li className="min-w-52 flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector text-xl">
+          <li className="min-w-52 flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector text-xl">
+          <div className="flex flex-row items-center justify-center">
             <Image src={LogoSister} alt="Logo" width={40} className="mx-4" />
-            Nauan
+            <h1>Nauan</h1>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <div className="border-1 border-white-700 h-8"></div>
+              <h1 className="mr-5 ml-3">2</h1>
+            </div>
           </li>
-          <li className="text-xl flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
+          <li className="text-xl flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
+          <div className="flex flex-row items-center justify-center">
             <Image src={LogoSister} alt="Logo" width={40} className="mx-4" />
-            Jessé
+            <h1>Jessé</h1>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <div className="border-1 border-white-700 h-8"></div>
+              <h1 className="mr-5 ml-3">1</h1>
+            </div>
           </li>
           <li className="text-xl flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
           <div className="flex flex-row items-center justify-center">
@@ -79,59 +139,93 @@ export default function VoleiAvancadoMasc() {
           </li>
         </ol>
         <ol className="text-xl flex flex-1 flex-col justify-around mr-5 ml-5 round">
-          <li className="min-w-44 com-connect flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
-            <Image src={QuestionMark} alt="Logo" width={35} className="mx-4" />
-            Vencedor
+          <li className="min-w-52 com-connect flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
+          <div className="flex flex-row items-center justify-center">
+            <Image src={LogoSister} alt="Logo" width={40} className="mx-4" />
+            <h1>Nauan</h1>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <div className="border-1 border-white-700 h-8"></div>
+              <h1 className="mr-5 ml-3">2</h1>
+            </div>
           </li>
           <li className="com-connect text-xl flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
           <div className="flex flex-row items-center justify-center">
             <Image src={LogoDs} alt="Logo" width={40} className="mx-4" />
             <h1>Isaac</h1>
             </div>
-            {/* <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-row items-center justify-center">
               <div className="border-1 border-white-700 h-8"></div>
-              <h1 className="mr-5 ml-3">2</h1>
-            </div> */}
+              <h1 className="mr-5 ml-3">0</h1>
+            </div>
           </li>
-          <li className="com-connect text-xl flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
+          <li className="com-connect text-xl flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
           <div className="flex flex-row items-center justify-center">
             <Image src={LogoDs} alt="Logo" width={40} className="mx-4" />
             <h1>Eliel</h1>
           </div>
-          {/* <div className="flex flex-row items-center justify-center">
+          <div className="flex flex-row items-center justify-center">
               <div className="border-1 border-white-700 h-8"></div>
-              <h1 className="mr-5 ml-3">2</h1>
-          </div> */}
+              <h1 className="mr-5 ml-3">1</h1>
+          </div>
           </li>
-          <li className="com-connect text-xl flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
+          <li className="com-connect text-xl flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
           <div className="flex flex-row items-center justify-center">
             <Image src={LogoSister} alt="Logo" width={40} className="mx-4" />
             <h1>Ary</h1>
             </div>
-            {/* <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-row items-center justify-center">
               <div className="border-1 border-white-700 h-8"></div>
               <h1 className="mr-5 ml-3">2</h1>
-            </div> */}
+            </div>
           </li>
         </ol>
         <ol className="flex flex-1 flex-col justify-around mr-5 ml-5 round">
-          <li className="min-w-44 text-xl com-connect flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
-            <Image src={QuestionMark} alt="Logo" width={35} className="mx-4" />
-            Vencedor
+          <li className="min-w-52 text-xl com-connect flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
+          <div className="flex flex-row items-center justify-center">
+            <Image src={LogoSister} alt="Logo" width={40} className="mx-4" />
+            <h1>Nauan</h1>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <div className="border-1 border-white-700 h-8"></div>
+              <h1 className="mr-5 ml-3">2</h1>
+            </div>
           </li>
-          <li className="com-connect text-xl flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
-            <Image src={QuestionMark} alt="Logo" width={35} className="mx-4" />
-            Vencedor
+          <li className="com-connect text-xl flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative with-connector">
+          <div className="flex flex-row items-center justify-center">
+            <Image src={LogoSister} alt="Logo" width={40} className="mx-4" />
+            <h1>Ary</h1>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <div className="border-1 border-white-700 h-8"></div>
+              <h1 className="mr-5 ml-3">1</h1>
+            </div>
           </li>
         </ol>
         <ol className="min-w-44 flex flex-1 flex-col justify-around mr-5 ml-5 round">
-          <li
+        <motion.li
             id="round-5"
-            className="text-xl min-w-44 flex items-center m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative finish"
+            ref={winnerRef}
+            onHoverStart={animateConfetti}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="min-w-52 text-xl flex items-center justify-between m-2 p-1 leading-relaxed bg-gray-600 text-gray-300 rounded-full relative finish"
           >
-            <Image src={QuestionMark} alt="Logo" width={35} className="mx-4" />
-            Vencedor
-          </li>
+            <div className="flex flex-row items-center justify-center">
+              <Image
+                src={LogoSister}
+                alt="LogoSister"
+                width={40}
+                className="mx-4"
+              />
+              <h1>Nauan</h1>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <h1 className={`mr-5 ml-3 ${
+                isShaking ? "animate-shake" : ""
+              }`}>🏆</h1>
+            </div>
+          </motion.li>
         </ol>
       </div>
     </div>
